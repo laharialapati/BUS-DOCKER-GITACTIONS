a9fb1f52216a33c872704e61e450bd1372aae8e2
lahari <2300031852@kluniversity.in>
2025-09-12 00:38:20 +0530

    modified some changes

---
+++ busbackend/BusServiceDemoProject/src/test/java/com/klef/dev/BusServiceProjectApplicationTests.java
@@ -0,0 +1,12 @@
+package com.klef.dev;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(classes = BusServiceProjectApplication.class)
+public class BusServiceProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
