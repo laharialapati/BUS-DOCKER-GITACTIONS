a5fb77c3950d17a9ea58e12d6bb788e9c864f7eb
laharialapati <144048003+laharialapati@users.noreply.github.com>
2025-09-23 15:19:33 +0530

    Update Bus.java

--- busbackend/BusServiceDemoProject/src/main/java/com/klef/dev/entity/Bus.java
+++ busbackend/BusServiceDemoProject/src/main/java/com/klef/dev/entity/Bus.java
@@ -4,6 +4,9 @@ import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+
 
 @Entity
 @Table(name = "bus_table")
